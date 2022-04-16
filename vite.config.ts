247af252e9fc1753884bf8e9ec81eeaa7fdfcdf9
Candid Dauth <cdauth@cdauth.eu>
2022-04-16 14:53:31 +0200

    Migrate to yarn+Vite and publish ESM bundle

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+import dtsPlugin from 'vite-plugin-dts';
+
+export default defineConfig({
+	plugins: [
+		cssInjectedByJsPlugin(),
+		dtsPlugin()
+	],
+	build: {
+		sourcemap: true,
+		minify: false,
+		lib: {
+			entry: './src/L.AutoGraticule.ts',
+			name: 'L.AutoGraticule',
+			fileName: (format) => `L.AutoGraticule.${format === 'umd' ? 'js' : 'mjs'}`
+		},
+		rollupOptions: {
+			output: {
+				globals: {
+					'leaflet': 'L'
+				}
+			},
+			external: ['leaflet']
+		}
+	}
+});
